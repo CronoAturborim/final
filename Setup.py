5ae41a6a4bf9e808be5e9ffe5fee3d8218620b21
CronoAturborim <136660397+CronoAturborim@users.noreply.github.com>
2023-06-14 23:53:47 -0500

    Add files via upload

--- Setup.py
+++ Setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fpec3",
-    version="0.1",
+    name="lols0",
+    version="0.2",
     license="MIT",
-    description="Cuestionario para reforzar tus conocimientos de python estilo juego trivia",
-    author="Franck Pérez C",
+    description="base de datos",
+    author="Andres Pérez C",
     packages=find_packages(),
-    url="https://github.com/franckp1/fpec3"
+    url="https://github.com/CronoAturborim/final"
 
 )
